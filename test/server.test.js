a2e908e3aeeac7af7679a190dec9f7bfed418ad8
proflucasgoulart <103764493+proflucasgoulart@users.noreply.github.com>
2022-04-27 00:42:29 -0300

    Delete server.test.js

--- test/server.test.js
+++
@@ -1,8 +0,0 @@
-const supertest = require('supertest');
-
-const request = supertest('http://localhost:5678');
-
-test('Servidor na porta 5678', async () => {
-  const resposta = await request.get('/');
-  expect(resposta.status).toBe(200);
-});
